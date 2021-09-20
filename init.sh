0100eb1f9f644a525b233c73f646a091b3481869
Albert Bhagwan <albert.bhagwan@gmail.com>
2021-09-20 10:27:36 +0200

    Until intro

---
+++ init.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+export MANPATH="$MANPATH:/usr/local/texlive/2021/texmf-dist/doc/man"
+export INFOPATH="$INFOPATH:/usr/local/texlive/2021/texmf-dist/doc/info"
+export PATH=/usr/local/texlive/2021/bin/x86_64-linux:$PATH
